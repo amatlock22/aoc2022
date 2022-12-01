758dd0a8c23d2d7a996e99d2338d96adc50dfd25
Alex Matlock <amatlock22@gmail.com>
2022-12-01 18:39:10 -0500

    day 1

---
+++ day1/day1.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("Part 1:", part1())
+	fmt.Println("Part 2:", part2())
+}
+
+func part1() int {
+	file, _ := os.Open("input.txt")
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	maxCals, currentCals := 0, 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 {
+			if currentCals > maxCals {
+				maxCals = currentCals
+			}
+
+			currentCals = 0
+
+			continue
+		}
+
+		cals, _ := strconv.Atoi(line)
+		currentCals += cals
+	}
+
+	return maxCals
+}
+
+func part2() int {
+	file, _ := os.Open("input.txt")
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	allCals := []int{}
+	currentCals := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 {
+			allCals = append(allCals, currentCals)
+			currentCals = 0
+
+			continue
+		}
+
+		cals, _ := strconv.Atoi(line)
+		currentCals += cals
+	}
+
+	sort.Slice(allCals, func(a, b int) bool {
+		return allCals[b] < allCals[a]
+	})
+
+	return allCals[0] + allCals[1] + allCals[2]
+}
